416f82345c219d6f1a385914b7e5d8ac68023b85
Pratpatom <pratpatom.s@gmail.com>
2024-02-09 16:12:45 +0700

    Add sorting articles in ascending order.

--- pubmedxml.py
+++ pubmedxml.py
@@ -3,9 +3,12 @@ import re
 import json
 
 
-with open("issue_list.json") as f:
-    data = json.load(f)
-ISSUE_LIST = data["items"]
+def load_list_of_issues(file_name):
+    with open(file_name) as f:
+        data = json.load(f)
+    return data["items"]
+
+ISSUE_LIST = load_list_of_issues("issue_list.json")
 
 def read_vol_issue(xml_data: str):
     tree_root = ET.fromstring(xml_data)
@@ -19,31 +22,27 @@ def get_issue_id(vol: int, issue: str):
             return item['id']
     return None
 
+def fix_title(root, data):
+
+    data_indx = 0
+
+    # Insert the new element after each Journal element
+    for article in root.findall("Article"):
+        # Create a new element
+        new_element = ET.Element('ArticleTitle')
+        # Set the text content of the new element
+        new_element.text = data["articles"][data_indx]["title"]
+        new_element.tail = '\n'
+        article.insert(1, new_element)
+        data_indx = data_indx + 1
+
 def fix_xml(xml_data: str, file_name, data_fix):
 
-    # Parse the XML file
-    # tree = ET.parse('./uploads/pubmed-20230830-153649-issues-476.xml')
-
-    # Get the root element
     root = ET.fromstring(xml_data)
-
-    # Modify the header element
-    # volume = root.find('Article/Journal/Volume')
-
-    # issue = root.find('Article/Journal/Issue')
-
     data = data_fix
-    #with open("data_fix.json", "r") as f:
-    #    data = json.load(f)
-
-    # publisher.text = 'Division of Innovation and Research, Department of Disease Control, Ministry of Public Health, Thailand'
 
     for publisher in root.findall('Article/Journal/PublisherName'):
         publisher.text = 'Department of Disease Control, Ministry of Public Health, Thailand'
-
-    # Find all Journal elements
-    # articles = root.findall('Article')
-
 
     for authorlist in root.findall('Article/AuthorList'):
         for author in authorlist.findall('Author'):
@@ -65,6 +64,20 @@ def fix_xml(xml_data: str, file_name, data_fix):
         for journal in article.findall('Journal'):
             journal.find('Issn').text = '1178-2005'
 
+    # Get all Article elements
+    articles = root.findall('Article')
+
+    # Sort the articles by the text content of the FirstPage element
+    sorted_articles = sorted(articles, key=lambda article: int(article.find('FirstPage').text))
+
+    # Remove the original Article elements from the root
+    for article in articles:
+        root.remove(article)
+
+    # Add the sorted Article elements to the root
+    for article in sorted_articles:
+        root.append(article)
+
     # Write the modified XML to a new file
     tree = ET.ElementTree(root)
     tree.write('modified_file.xml', encoding='utf-8', xml_declaration=False)
@@ -72,24 +85,9 @@ def fix_xml(xml_data: str, file_name, data_fix):
     with open('modified_file.xml', 'r', encoding='utf-8') as f:
         contents = f.read()
 
-    # h = '<!DOCTYPE ArticleSet PUBLIC "-//NLM//DTD PubMed 2.8//EN" "https://dtd.nlm.nih.gov/ncbi/pubmed/in/PubMed.dtd">\n' + contents
-    h = '<?xml version="1.0"?>\n<!DOCTYPE ArticleSet PUBLIC "-//NLM//DTD PubMed 2.0//EN" "http://www.ncbi.nlm.nih.gov/entrez/query/static/PubMed.dtd">\n' + contents
+    h = '<!DOCTYPE ArticleSet PUBLIC "-//NLM//DTD PubMed 2.8//EN" "https://dtd.nlm.nih.gov/ncbi/pubmed/in/PubMed.dtd">\n' + contents
 
-    with open(f'{file_name} modified_file.xml', 'w', encoding='utf-8') as f:
+    with open(f'{file_name}', 'w', encoding='utf-8') as f:
         f.write(h)
 
-    return f"{file_name} modified_file.xml"
-
-def fix_title(root, data):
-
-    data_indx = 0
-
-    # Insert the new element after each Journal element
-    for article in root.findall("Article"):
-        # Create a new element
-        new_element = ET.Element('ArticleTitle')
-        # Set the text content of the new element
-        new_element.text = data["articles"][data_indx]["title"]
-        new_element.tail = '\n'
-        article.insert(1, new_element)
-        data_indx = data_indx + 1
+    return f"{file_name}"
